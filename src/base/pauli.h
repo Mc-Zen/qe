c524b8259fb9aeba27ff6f5a9863166c775b176e
Mc-Zen <alphabetagamma11@hotmail.com>
2024-04-13 21:11:56 +0200

    [add] Pauli class

--- src/base/pauli.h
+++ src/base/pauli.h
@@ -2,17 +2,65 @@
 #pragma once
 #include <cstddef>
 #include <vector>
+#include <string_view>
+#include <format>
+#include <string>
+
+#include "binary.h"
+#include "binary_phase.h"
 
 
 namespace qe {
 
-	class Pauli {
-	public:
-		explicit Pauli(int num_qubits) {}
+class Pauli {
+public:
+	explicit Pauli(int num_qubits) : x_vector(num_qubits), z_vector(num_qubits) {}
+	explicit Pauli(std::string_view pauli_string) { from_pauli_string(pauli_string); }
+	static Pauli Single_X(int num_qubits, int qubit);
+	static Pauli Single_Z(int num_qubits, int qubit);
 
-	private:
-		std::vector<std::byte> x;
-		std::vector<std::byte> z;
-	};
+	int num_qubits() const { return static_cast<int>(x_vector.size()); }
+	BinaryPhase phase() const { return phase_; }
 
-}
+	/// @brief Check if operator at given qubit has an x component
+	int x(int qubit) const { return x_vector[qubit]; }
+	/// @brief Check if operator at given qubit has a z component
+	int z(int qubit) const { return z_vector[qubit]; };
+
+
+	/// @brief Check if operator at given qubit has an x component
+	void set_x(int qubit, int value) { x_vector[qubit] = value; }
+	/// @brief Check if operator at given qubit has a z component
+	void set_z(int qubit, int value) { z_vector[qubit] = value; };
+
+	void set_phase(BinaryPhase phase) { phase_ = phase; };
+
+	friend bool operator==(const Pauli& a, const Pauli& b) = default;
+
+	std::string to_string(bool add_phase=true) const;
+
+private:
+
+	void from_pauli_string(std::string_view pauli_string);
+	void from_phaseless_pauli_string(std::string_view pauli_string);
+	int count_Y_paulis() const;
+
+	std::vector<Binary> x_vector;
+	std::vector<Binary> z_vector;
+	BinaryPhase phase_;
+};
+
+
+class FPauli {};
+
+} // namespace qe
+
+
+
+template <class CharT>
+struct std::formatter<qe::Pauli, CharT> : std::formatter<std::string_view, CharT> {
+	template <class FormatContext>
+	auto format(const qe::Pauli& pauli, FormatContext& fc) const {
+		return std::format_to(fc.out(), "{}", pauli.to_string());
+	}
+};
--- src/base/tests/pauli_tests.cpp
+++ src/base/tests/pauli_tests.cpp
@@ -1,8 +1,49 @@
-﻿#include "catch2/catch_test_macros.hpp"
-#include "catch2/catch_approx.hpp"
+﻿#include "catch2/catch_approx.hpp"
+#include "catch2/catch_test_macros.hpp"
 
+#include "matrix.h"
 #include "pauli.h"
 #include <iostream>
 
 using namespace qe;
 
+TEST_CASE("Pauli num_qubits constructor") {
+	auto p = Pauli(7);
+	REQUIRE(p.num_qubits() == 7);
+	for (int i = 0; i < p.num_qubits(); ++i) {
+		REQUIRE(p.x(i) == 0);
+		REQUIRE(p.z(i) == 0);
+	}
+}
+
+TEST_CASE("Pauli string constructor") {
+	auto p = Pauli("XYZ");
+	REQUIRE(p.phase() == 1);
+
+	p = Pauli("iXYZ");
+	REQUIRE(p.phase() == 2);
+
+	p = Pauli("-XYZ");
+	REQUIRE(p.phase() == 3);
+
+	p = Pauli("-iXYZ");
+	REQUIRE(p.phase() == 0);
+}
+
+TEST_CASE("Pauli set X/Z") {
+	auto p = Pauli("-XYZ");
+	p.set_x(0, 0);
+	p.set_z(0, 1);
+	REQUIRE(p.x(0) == 0);
+	REQUIRE(p.z(0) == 1);
+}
+
+TEST_CASE("Format Paulis") {
+	REQUIRE(std::format("{}", Pauli{ "iXYYZ" }) == "iXYYZ");
+	REQUIRE(std::format("{}", Pauli{ "X" }) == "+X");
+	REQUIRE(std::format("{}", Pauli{ "+X" }) == "+X");
+	REQUIRE(std::format("{}", Pauli{ "iX" }) == "iX");
+	REQUIRE(std::format("{}", Pauli{ "-iIX" }) == "-iIX");
+	REQUIRE(std::format("{}", Pauli{ "-IY" }) == "-IY");
+	REQUIRE(std::format("{}", Pauli{ "-IYY" }) == "-IYY");
+}
